32a84fdc46fdacea0c631a554d388d31291af489
PedroFRomao <pedroferraz.romao@gmail.com>
2023-05-08 12:14:24 -0300

    commit save

--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
 //titulo
-window.onload = function() {
-    var canvas = document.getElementById("UgCanvas");
-    var context = canvas.getContext("2d");
+var canvas = document.getElementById("UgCanvas");
+var ctx = canvas.getContext("2d");
 
-    context.font = "italic 40pt arial";
-    context.fillText("Teste de elelmentos do canva", 100, 100);
-};
+ctx.font = "italic 40pt arial";
+ctx.fillText("Teste de elelmentos do canva", 100, 100);
+
 
 //retangulo
-var c=document.getElementById("UgCanvas");
-var ctx=c.getContext("2d");
 ctx.fillStyle="#7F7F00";
 ctx.fillRect(50,150,250,200);
 
 //circulo
-var c=document.getElementById("UgCanvas");
-var ctx=c.getContext("2d");
 ctx.fillStyle="#7F7F00";
 ctx.beginPath();
-ctx.arc(450,250,50,100,Math.PI*2,true);
+ctx.arc(450,250,70,100,Math.PI*2,true);
 ctx.closePath();
 ctx.fill();
 
 //linha
-var c=document.getElementById("UgCanvas");
-var ctx=c.getContext("2d");
-ctx.moveTo(550,250);
+ctx.moveTo(600,250);
 ctx.lineTo(650,150);
-ctx.lineTo(700,250);
+ctx.lineTo(750,200);
 ctx.stroke();
 
-//teste
+//imagem
+var img=new Image();
+img.onload = function(){
+ctx.drawImage(img,100,150);
+};
+img.src="img/img.png";
