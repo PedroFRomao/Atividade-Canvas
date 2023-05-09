f7f489c755bbed2855a7e886ef7be6c86b00416d
PedroFRomao <pedroferraz.romao@gmail.com>
2023-05-09 09:18:27 -0300

    adicao da curva

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
 //titulo
 var canvas = document.getElementById("UgCanvas");
 var ctx = canvas.getContext("2d");
-
+ctx.fillStyle="#227bb5";
 ctx.font = "italic 40pt arial";
 ctx.fillText("Teste de elelmentos do canva", 100, 100);
 
-
 //retangulo
-ctx.fillStyle="#7F7F00";
+ctx.fillStyle="#227bb5";
 ctx.fillRect(50,150,250,200);
 
 //circulo
-ctx.fillStyle="#7F7F00";
+ctx.fillStyle="#227bb5";
 ctx.beginPath();
-ctx.arc(450,250,70,100,Math.PI*2,true);
+ctx.arc(450,250,80,100,Math.PI*2,true);
 ctx.closePath();
 ctx.fill();
+
+//curva
+ctx.beginPath();
+ctx.arc(680,300,80,0,Math.PI);
+ctx.stroke();
 
 //linha
 ctx.moveTo(600,250);
@@ -26,6 +30,6 @@ ctx.stroke();
 //imagem
 var img=new Image();
 img.onload = function(){
-ctx.drawImage(img,100,150);
+ctx.drawImage(img,150,450);
 };
 img.src="img/img.png";
